0884fc5a6f3a7e3e1adc6fcd7d0a6701d414e969
Shub17-code <sc241617@gmail.com>
2026-01-14 21:43:47 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
   const login = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("http://localhost:5000/api/auth/login", {
+    const res = await fetch("https://gig-flow-ejdn.onrender.com/api/auth/login", {
       method: "POST",
       credentials: "include",
       headers: { "Content-Type": "application/json" },
